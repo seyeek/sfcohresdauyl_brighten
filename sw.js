2c84267e87b6f3681cbfb1f0ce3ff218ece92a5b
Myschedul Dev <user@myschedul.local>
2026-01-09 16:27:31 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for MySchedul PWA
-const CACHE_NAME = 'myschedul-v8';
+const CACHE_NAME = 'myschedul-v9';
 
 // Install - skip waiting
 self.addEventListener('install', (event) => {
